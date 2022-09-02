c8e4f755b02d4c3d830f8e4ad9488432ecc9011d
Rajat Dubey <72385522+rajatdubey123@users.noreply.github.com>
2022-09-02 12:06:32 +0530

    Time: 1 ms (98.71%), Space: 41.5 MB (32.65%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int fast = n;
+        int slow = n;
+        do{
+            slow = findsquare(slow);
+            fast = findsquare(findsquare(fast));
+        }while(slow != fast);
+            
+            if(slow == 1){
+                return true;
+            }
+        return false;
+    }
+         private int findsquare(int number){
+            int ans =0;
+            while(number>0){
+                int rem = number % 10;
+                ans += rem * rem;
+                number = number/10;
+            }
+            return ans;
+        }
+    }
