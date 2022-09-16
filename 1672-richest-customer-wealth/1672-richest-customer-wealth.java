0250c2c686ce21a146d560e76e34ee41fa8ecc3a
Rajat Dubey <72385522+rajatdubey123@users.noreply.github.com>
2022-09-17 00:17:07 +0530

    Time: 1 ms (59.00%), Space: 43.4 MB (45.83%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,15 +1,14 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int m = accounts.length;
-        int sum = 0;
-        int richest = 0;
-        for(int i = 0; i<m; i++){
-            for(int j=0; j<accounts[i].length; j++){
-               sum = sum + accounts[i][j];
-                }
-            richest = Math.max(richest,sum);
-            sum =0;
+        
+        int max1= Integer.MIN_VALUE;
+        for(int i=0; i<accounts.length; i++){
+            int sum =0;
+            for(int j =0; j<accounts[i].length; j++){
+                sum = sum + accounts[i][j];
             }
-        return richest;
+            max1 = Math.max(max1,sum);
+        }
+        return max1;
     }
 }
