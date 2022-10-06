bbbbf2f951eb67412a914603f44bb0fdbda938d6
Rajat Dubey <72385522+rajatdubey123@users.noreply.github.com>
2022-10-07 00:23:52 +0530

    Time: 1 ms (99.17%), Space: 44.3 MB (56.28%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -21,9 +21,9 @@ class Solution {
         if (root == null) return 0;
         
         int leftHeight = dfsHeight (root.left);
-        if (leftHeight == -1) return -1;
+         if (leftHeight == -1) return -1;
         int rightHeight = dfsHeight (root.right);
-        if (rightHeight == -1) return -1;
+         if (rightHeight == -1) return -1;
         
         if (Math.abs(leftHeight - rightHeight) > 1)  return -1;
         return Math.max(leftHeight, rightHeight) + 1;
