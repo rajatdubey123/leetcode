906adb05185f52e64345c155af8521f5123c7ee8
Rajat Dubey <72385522+rajatdubey123@users.noreply.github.com>
2022-10-08 00:33:35 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (89.15%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,27 @@
 class Solution {
     public int fib(int n) {
-        int [] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        if(n == 0){
+        // int [] dp = new int[n+1];
+        // Arrays.fill(dp,-1);
+        // if(n == 0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        // if(dp[n] != -1)
+        //     return dp[n];
+        // return dp[n] = fib(n-1) + fib(n-2);
+        int prev2 = 0;
+        int prev = 1;
+        if(n==0)
             return 0;
-        }
-        if(n==1){
+        if(n==1)
             return 1;
+        for(int i=2; i<=n; i++){
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
         }
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = fib(n-1) + fib(n-2);
-        
+        return prev;
     }
 }
