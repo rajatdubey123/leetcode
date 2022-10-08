9a17f47ec98f3714d8924045614c8c66d99afabc
Rajat Dubey <72385522+rajatdubey123@users.noreply.github.com>
2022-10-08 22:30:12 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (68.36%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int [] dp = new int[nums.length +1];
+        Arrays.fill(dp,-1);
+        int i = nums.length -1;
+        return f(nums,i,dp);
+        
+    }
+    public int f(int[] nums, int i,int []dp){
+        if(i < 0){
+            return 0;
+        }
+        if(dp[i] != -1)
+            return dp[i];
+         dp[i] = Math.max(f(nums,i-2,dp) + nums[i], f(nums,i-1,dp));
+        return dp[i];
+    }
+}
